a11fc5912014147ce296e838a70d164b51b30cc0
Dias <rexusfork@gmail.com>
2025-06-24 09:39:00 +0700

    add sign up handler

---
+++ internals/handlers/membership/signup.go
@@ -0,0 +1,29 @@
+package membership
+
+import (
+	"net/http"
+
+	"simple-forum/internals/models/membership"
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) Signup(c *gin.Context) {
+	context := c.Request.Context()
+
+	var request membership.SignUpRequest
+	err := c.ShouldBindJSON(&request)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	err = h.membershipService.Signup(context, request)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return 
+	}
+}
